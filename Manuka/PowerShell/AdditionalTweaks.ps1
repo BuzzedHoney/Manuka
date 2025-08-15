eff0ca4f294ab4f254eceb2cacbea9952187fac1
Buzzed <174978011+BuzzedHoney@users.noreply.github.com>
2025-08-15 09:12:30 -1200

    Update AdditionalTweaks.ps1

--- Manuka/PowerShell/AdditionalTweaks.ps1
+++ Manuka/PowerShell/AdditionalTweaks.ps1
@@ -1,4 +1,7 @@
-# I'll add more if I find some...
+# I'll add more if I find some... If you have any ideas create an issue and I'll look into whatever registry or service tweak you give me.
 Write-Host "Configuring Registry Tweaks"
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "MenuShowDelay" -Type String -Value "0"
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ExtendedUIHoverTime" -Type DWord -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CrossDeviceResume\Configuration" -Name "IsResumeAllowed" -Type DWord -Value 0
+
+# sc.exe config "WSearch" start= disabled
