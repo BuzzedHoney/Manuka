7ad92cfc1b28b98392805d54f3be10d7140f80d0
Buzzed <174978011+BuzzedHoney@users.noreply.github.com>
2025-10-26 21:45:44 -0500

    Delete Manuka/PowerShell/AdditionalTweaks.ps1

--- Manuka/PowerShell/AdditionalTweaks.ps1
+++
@@ -1,4 +0,0 @@
-# I'll add more if I find some... If you have any ideas create an issue and I'll look into whatever registry or service tweak you give me.
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "MenuShowDelay" -Type String -Value "0"
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ExtendedUIHoverTime" -Type DWord -Value 1
-# sc.exe config "WSearch" start= disabled
